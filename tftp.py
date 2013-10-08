cf615d0ad1d2a640a4c71aa7a8f75f168d12d4dd
Haukur Rosinkranz <hauxir@gmail.com>
2013-10-08 01:16:06 +0000

    read

--- tftp.py
+++ tftp.py
@@ -3,21 +3,54 @@
 import sys
 import os
 import socket
+import struct
 
-
-def req_header(filename,opcode,mode):
-  if opcode == "RRQ":
-    codehex = "\x00\x01"
-  elif opcode == "WRQ":
-    codehex = "\x00\x02"
-  return codehex + filename + "\x00" + mode + "\x00"
 
 def read(host,port,filename):
-  s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-  s.connect((host, port))
-  header = req_header(filename,"WRQ","octet")
-  print header
-  s.sendto(header,(host,port))
+  clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  clientsocket.settimeout(5)
+  pack_format = '!H' + str(len(filename)) + 'sB5sB'
+  sendpacket = struct.pack(pack_format.encode(), 1, filename.encode(), 0, b'octet', 0)
+  clientsocket.sendto(sendpacket, (host, port))
+  totaldatalen = 0
+  countblock = 1
+  while True:
+      retries = 0
+      while retries < 3:
+          try:
+            data, remotesocket = clientsocket.recvfrom(512)
+            opcode = struct.unpack('!H', data[0:2])[0]
+            retries= 0
+            break
+          except:
+            clientsocket.sendto(sendpacket, (host, port))
+            opcode = 'Timeout'
+            retries += 1
+      if opcode == 3:
+        blockno = struct.unpack('!H',data[2:4])[0]
+        if blockno != countblock:
+            clientsocket.sendto(errBlockNo, remotesocket)
+            print('Receive wrong block. Session closed.')
+            break
+        countblock += 1
+        payload = data[4:]
+        totaldatalen += len(payload)
+        sendpacket = struct.pack(b'!2H', 4, blockno)
+        clientsocket.sendto(sendpacket, remotesocket)
+        if len(payload) < 512:
+            print('\rget %s :%s bytes. finish.' %(filename, totaldatalen))
+            break
+      elif opcode == 5:
+        errString = data[4:-1]
+        print('Received error code %s : %s' %(str(errCode), bytes.decode(errString)))
+        break
+      elif opcode == 'Timeout':
+        print('Timeout. Session closed.')
+        break
+      else:
+        print('Unknown error. Session closed.')
+        break
+
 
 def write(host,port,filename):
   if not os.path.isfile(filename):
